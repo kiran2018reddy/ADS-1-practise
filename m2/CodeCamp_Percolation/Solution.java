19749934befdd5da81889c6d4a43c198b373e995
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-09-30 23:02:47 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

---
+++ m2/CodeCamp_Percolation/Solution.java
@@ -0,0 +1,37 @@
+// public class Percolation {
+//    public Percolation(int n)                // create n-by-n grid, with all sites blocked
+//    public    void open(int row, int col)    // open site (row, col) if it is not open already
+//    public boolean isOpen(int row, int col)  // is site (row, col) open?
+//    public boolean isFull(int row, int col)  // is site (row, col) full?
+//    public     int numberOfOpenSites()       // number of open sites
+//    public boolean percolates()              // does the system percolate?
+// }
+
+
+// You can implement the above API to solve the problem
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+}
+
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        Percolation percolation = new Percolation(size);
+        while (scan.hasNext()) {
+            percolation.open(scan.nextInt(), scan.nextInt());
+        }
+        System.out.println(percolation.percolates());
+    }
+}
