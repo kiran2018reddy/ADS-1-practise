de29d10a2bfc44adef8cd748e264cab725a7832c
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-10-06 12:18:32 +0530

    "check_style_score -> 2 of 6 passed. style score: 0/1 "

--- m12/Assignment-1/Solution.java
+++ m12/Assignment-1/Solution.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 class Insertionsort {
-	void ex(studentdata[] array, int sindex, int bindex) {
-		studentdata tmp = array[sindex];
-		array[sindex] = array[bindex];
-		array[bindex] = tmp;
+	void exchange(studentdata[] array, int smallindex, int bigindex) {
+		studentdata temp = array[smallindex];
+		array[smallindex] = array[bigindex];
+		array[bigindex] = temp;
 	}
-	boolean less(studentdata d1, studentdata d2) {
-		return d1.compareTo(d2) == -1;
+	boolean less(studentdata valueone, studentdata valuetwo) {
+		return valueone.compareTo(valuetwo) == -1;
 	}
 	void sort(studentdata[] students) {
-		for (int i = 1; i < students.length; i = i + 1) {
-			for (int j = i; j > 0; j = j + 1) {
+		for (int i = 1; i < students.length; i++) {
+			for (int j = i; j > 0; j--) {
 				if (less(students[j - 1], students[j])) {
-					ex(students, j - 1, j);
+					exchange(students, j - 1, j);
 				} else {
 					break;
 				}
@@ -23,18 +23,18 @@ class Insertionsort {
 class studentdata implements Comparable<studentdata> {
 	String studentname;
 	String birthdate;
-	int s1;
-	int s2;
-	int s3;
-	int tmarks;
+	int sub1marks;
+	int sub2marks;
+	int sub3marks;
+	int totalmarks;
 	String rescategory;
-	studentdata(String name, String dob, int s1, int s2, int s3, int tmarks, String category) {
+	studentdata(String name, String dob, int sub1, int sub2, int sub3, int total, String category) {
 		this.studentname = name;
 		this.birthdate = dob;
-		this.s1 = s1;
-		this.s2 = s2;
-		this.s3 = s3;
-		this.tmarks = tmarks;
+		this.sub1marks = sub1;
+		this.sub2marks = sub2;
+		this.sub3marks = sub3;
+		this.totalmarks = total;
 		this.rescategory = category;
 	}
 	String[] datesplit() {
@@ -42,22 +42,22 @@ class studentdata implements Comparable<studentdata> {
 		return splitdate;
 	}
 	public int compareTo(studentdata other) {
-		if (this.tmarks > other.tmarks) {
+		if (this.totalmarks > other.totalmarks) {
 			return 1;
 		}
-		if (this.tmarks < other.tmarks) {
+		if (this.totalmarks < other.totalmarks) {
 			return -1;
 		}
-		if (this.s3 > other.s3) {
+		if (this.sub3marks > other.sub3marks) {
 			return 1;
 		}
-		if (this.s3 < other.s3) {
+		if (this.sub3marks < other.sub3marks) {
 			return -1;
 		}
-		if (this.s2 > other.s2) {
+		if (this.sub2marks > other.sub2marks) {
 			return 1;
 		}
-		if (this.s2 < other.s2) {
+		if (this.sub2marks < other.sub2marks) {
 			return -1;
 		}
 		if (Integer.parseInt(this.datesplit()[2]) > Integer.parseInt(other.datesplit()[2])) {
@@ -86,42 +86,73 @@ class Solution {
 	Solution() {
 		//unused
 	}
-	public static void meritofstudent(studentdata[] array, int vacancys, int open, int bc, int sc, int st) {
+	static int opencount = 0;
+	static int bccount = 0;
+	static int sccount = 0;
+	static int stcount = 0;
+	public static void countcat(studentdata[] array) {
 		for (int i = 0; i < array.length; i++) {
-			if (vacancys == 0) {
+			if (array[i].rescategory.equals("Open")) {
+				opencount++;
+			}
+			if (array[i].rescategory.equals("BC")) {
+				bccount++;
+			}
+			if (array[i].rescategory.equals("SC")) {
+				sccount++;
+			}
+			if (array[i].rescategory.equals("ST")) {
+				stcount++;
+			}
+		}
+	}
+	public static void meritfill(studentdata[] array, int vacancy, int open, int bc, int sc, int st) {
+		for (int i = 0; i < array.length; i++) {
+			if (vacancy == 0) {
+				// System.out.println("entered");
 				return;
 			}
 			if (open > 0) {
-				System.out.println(array[i].studentname + "," + array[i].tmarks + "," +
+				System.out.println(array[i].studentname + "," +
+				                   array[i].totalmarks + "," +
 				                   array[i].rescategory);
+				// if (array[i].rescategory.equals("BC")) {
+				// 	bc--;
+				// } else if(array[i].rescategory.equals("SC")) {
+				// 	sc--;
+				// } else if(array[i].rescategory.equals("ST")) {
+				// 	st--;
+				// }
 				open--;
-				vacancys--;
+				vacancy--;
 
 			} else {
 				if (array[i].rescategory.equals("BC") || array[i].rescategory.equals("SC") || array[i].rescategory.equals("ST")) {
 					if (array[i].rescategory.equals("BC") && bc > 0) {
 						System.out.println(array[i].studentname + "," +
-						                   array[i].tmarks + "," +
+						                   array[i].totalmarks + "," +
 						                   array[i].rescategory);
 						bc--;
-						vacancys--;
-					}
-					else if (array[i].rescategory.equals("SC") && sc > 0) {
+						vacancy--;
+					} else if (array[i].rescategory.equals("SC") && sc > 0) {
 						System.out.println(array[i].studentname + "," +
-						                   array[i].tmarks + "," +
+						                   array[i].totalmarks + "," +
 						                   array[i].rescategory);
 						sc--;
-						vacancys--;
-					}
-					else if (array[i].rescategory.equals("ST") && st > 0) {
+						vacancy--;
+					} else if (array[i].rescategory.equals("ST") && st > 0) {
 						System.out.println(array[i].studentname + "," +
-						                   array[i].tmarks + "," +
+						                   array[i].totalmarks + "," +
 						                   array[i].rescategory);
 						st--;
-						vacancys--;
+						vacancy--;
+					} else if (vacancy > 0 && bccount == 0 && sccount == 0 && stcount == 0) {
+						System.out.println(array[i].studentname + "," +
+						                   array[i].totalmarks + "," +
+						                   array[i].rescategory);
+						vacancy--;
 					}
 				}
-				
 			}
 		}
 	}
@@ -129,7 +160,7 @@ class Solution {
 	public static void print(studentdata[] array) {
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i].studentname + "," +
-			                   array[i].tmarks + "," +
+			                   array[i].totalmarks + "," +
 			                   array[i].rescategory);
 		}
 	}
@@ -154,7 +185,7 @@ class Solution {
 		insertion.sort(students);
 		print(students);
 		System.out.println();
-		meritofstudent(students, vacancynum, openvacancy, bcvacancy, scvacancy, stvacancy);
+		meritfill(students, vacancynum, openvacancy, bcvacancy, scvacancy, stvacancy);
 	}
 
 }
