167cc6f743dfd931fbca9efacd12193ff7564de6
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-10-13 13:02:51 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- m18/Week-3_Assignment-1/Solution.java
+++ m18/Week-3_Assignment-1/Solution.java
@@ -96,6 +96,7 @@ stMaxPQ.insert(new Stockexc(tokens[0], Float.parseFloat(tokens[1])));
                 }
             default:break;
       case "intersection":
+      System.out.println(symbolMax.get(tokens[2]));
         break;
             }
         }
@@ -213,8 +214,7 @@ class STable<Key extends Comparable<Key>, Value> {
     	for (int i = 0; i < keys.length; i++) {
             if (keys[i] != null) {
             System.out.println(keys[i] + " " + values[i]);
-
-            }
+        }
         }
     }
 }
