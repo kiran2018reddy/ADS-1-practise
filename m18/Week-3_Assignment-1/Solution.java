8cab95efe9bfc502b1de7c06f3829ee10cad232f
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-10-13 12:34:00 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- m18/Week-3_Assignment-1/Solution.java
+++ m18/Week-3_Assignment-1/Solution.java
@@ -10,10 +10,16 @@ class Stockexc implements Comparable<Stockexc>{
 	}
 
 	public String toString() {
-		return this.name+" "+this.data;
+		return this.name + " " + this.data;
 	}
-
-	public int compareTo(Stockexc that) {
+/**
+ * { function_description }.
+ *
+ * @param      that  The that.
+ *
+ * @return     { description_of_the_return_value }.
+ */
+	public int compareTo(final Stockexc that) {
 		if (this.data > that.data) {
             return +1;
         }
@@ -23,29 +29,31 @@ class Stockexc implements Comparable<Stockexc>{
         return this.name.compareTo(that.name);
     }
 }
-
+/**
+ * Class for solution.
+ */
 class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner input = new Scanner(System.in);
         int n = Integer.parseInt(input.nextLine());
         STable<String, Integer> symbolMax = new STable<String, Integer>(30);
         STable<String, Integer> symbolMin = new STable<String, Integer>(30);
 
-        for(int i = 0; i<6; i++) {
+        for (int i = 0; i < 6; i++) {
             MinPQ<Stockexc> stMinPQ = new MinPQ<Stockexc>();
             MaxPQ<Stockexc> stMaxPQ = new MaxPQ<Stockexc>();
-            for(int j = 0; j<n; j++) {
+            for (int j = 0; j < n; j++) {
                 String[] tokens = input.nextLine().split(",");
-                stMinPQ.insert(new Stockexc(tokens[0], Float.parseFloat(tokens[1])));
-                stMaxPQ.insert(new Stockexc(tokens[0], Float.parseFloat(tokens[1])));
+stMinPQ.insert(new Stockexc(tokens[0], Float.parseFloat(tokens[1])));
+stMaxPQ.insert(new Stockexc(tokens[0], Float.parseFloat(tokens[1])));
             }
-            for(int k = 0; k<5; k++) {
+            for (int k = 0; k < 5; k++) {
                 Stockexc a = stMaxPQ.deleteMax();
                 System.out.println(a);
                 symbolMax.put(a.name, 1);
             }
             System.out.println();
-            for(int k = 0; k<5; k++) {
+            for (int k = 0; k < 5; k++) {
                 Stockexc b = stMinPQ.deleteMin();
                 System.out.println(b);
                 symbolMin.put(b.name, 1);
@@ -54,21 +62,26 @@ class Solution {
         }
 
         int k = Integer.parseInt(input.nextLine());
-        for(int i = 0; i<k; i++) {
+        for (int i = 0; i < k; i++) {
             String[] tokens = input.nextLine().split(",");
-            switch(tokens[0]) {
+            switch (tokens[0]) {
                 case "get":
-                if(tokens[1].equals("maxST")) {
+                if (tokens[1].equals("maxST")) {
                     System.out.println(symbolMax.get(tokens[2]));
                 }
-                if(tokens[1].equals("minST")) {
+                if (tokens[1].equals("minST")) {
                     System.out.println(symbolMin.get(tokens[2]));
                 }
             }
         }
+    }
 }
-
-}
+/**
+ * Class for s table.
+ *
+ * @param      <Key>    The key.
+ * @param      <Value>  The value.
+ */
 class STable<Key extends Comparable<Key>, Value> {
     /**
      * key array of type key.
@@ -94,7 +107,12 @@ class STable<Key extends Comparable<Key>, Value> {
         keys = (Key[]) new Comparable[size];
         values =  new int[size];
       }
-
+/**
+ * { function_description }.
+ *
+ * @param      key    The key.
+ * @param      value  The value.
+ */
     public void put(final Key key, final int value) {
         int i = rank(key);
         if (i < n && keys[i].compareTo(key) == 0) {
@@ -109,7 +127,13 @@ class STable<Key extends Comparable<Key>, Value> {
         values[i] = value;
         n++;
     }
-
+/**
+ * { function_description }.
+ *
+ * @param      key   The key.
+ *
+ * @return     { description_of_the_return_value }.
+ */
     public int rank(final Key key) {
         int low = 0, high = n - 1;
         while (low <= high) {
@@ -125,7 +149,13 @@ class STable<Key extends Comparable<Key>, Value> {
         }
         return low;
     }
-
+/**
+ * { function_description }.
+ *
+ * @param      key   The key.
+ *
+ * @return     { description_of_the_return_value }.
+ */
     public int get(final Key key) {
         if (key == null) {
             System.out.println("No key");
@@ -140,14 +170,20 @@ class STable<Key extends Comparable<Key>, Value> {
         }
         return 0;
     }
-
+/**
+ * Determines if empty.
+ *
+ * @return     True if empty, False otherwise.
+ */
     public boolean isEmpty() {
         if (n != 0) {
             return false;
         }
         return true;
     }
-
+/**
+ * { function_description }.
+ */
     public void print() {
     	for (int i = 0; i < keys.length; i++) {
             if (keys[i] != null) {
